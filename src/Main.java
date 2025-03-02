b391fe68821e6d1c2888165d87d58c8a7bee4df6
hero534 <goldsmhr@mail.uc.edu>
2025-03-01 20:11:18 -0500

    Task 03 Completed

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         int num = 0;
-            for (num = 30; num >= 0; num--) {
+            for (num = 0; num <= 18; num += 3) {
                 System.out.print(num);
             }
     }
