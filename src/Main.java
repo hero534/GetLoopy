6094c55ed8983de615beb49c41f74f83ef87438e
hero534 <goldsmhr@mail.uc.edu>
2025-03-01 22:46:52 -0500

    Task 06 Completed

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         int col;
 
         for (row = 1; row <= 5; row++) {
-            for (col = 1; col <= row; col++) {
+            for (col = 5; col >= row; col--) {
                 System.out.print("*");
             }
             System.out.println();
